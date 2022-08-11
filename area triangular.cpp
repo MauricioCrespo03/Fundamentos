91b2190be565f2fb0de6582b13a9c5c3670a817c
MauricioCrespo03 <110415055+MauricioCrespo03@users.noreply.github.com>
2022-08-11 09:56:49 -0400

    Add files via upload

---
+++ area triangular.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main () {
+int b,h,a;
+cout<<"base";
+cin>>b;
+cout<<"altura";
+cin>>h;
+a=(b*h)/2;
+cout<<"area: "<<a<<endl;
+
+return 0; 
+}
